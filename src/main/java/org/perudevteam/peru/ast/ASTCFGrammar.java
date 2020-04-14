f6a6d3c7494fa7a3092e7873ac0d6a971be54dfb
chatham-abate <chathamabate@gmail.com>
2020-04-14 11:15:32 -0400

    AST CF Grammar Template Class.

---
+++ src/main/java/org/perudevteam/peru/ast/ASTCFGrammar.java
@@ -0,0 +1,16 @@
+package org.perudevteam.peru.ast;
+
+import io.vavr.CheckedFunction2;
+import io.vavr.collection.Map;
+import io.vavr.collection.Seq;
+import org.perudevteam.lexer.charlexer.CharData;
+import org.perudevteam.parser.grammar.SemanticCFGrammar;
+
+public class ASTCFGrammar<NT extends Enum<NT>, T extends Enum<T>>
+        extends SemanticCFGrammar<NT, T, ASTProduction<NT, T>, String, CharData<T>, AST> {
+
+    public ASTCFGrammar(NT s, Map<? super T, ? extends CheckedFunction2<String, CharData<T>, AST>> termGens,
+                        Seq<ASTProduction<NT, T>> prods) {
+        super(s, termGens, prods);
+    }
+}
