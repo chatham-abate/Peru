cc56d8a3e6b14bcaa431d8e808613cc758cbc46f
chatham-abate <chathamabate@gmail.com>
2020-04-14 10:37:30 -0400

    AST Line Exception

--- src/main/java/org/perudevteam/peru/ast/AST.java
+++ src/main/java/org/perudevteam/peru/ast/AST.java
@@ -3,20 +3,21 @@ package org.perudevteam.peru.ast;
 import io.vavr.Tuple2;
 import io.vavr.collection.Map;
 import io.vavr.control.Try;
+import org.perudevteam.misc.LineException;
 import org.perudevteam.peru.base.BaseValue;
 
 import java.util.Objects;
 
 @FunctionalInterface
 public interface AST  {
-    Tuple2<Map<String, BaseValue>, BaseResult> executeUnchecked(Map<String, BaseValue> env) throws Throwable;
+    Tuple2<Map<String, BaseValue>, BaseResult> executeUnchecked(Map<String, BaseValue> env) throws LineException;
 
     default Try<Tuple2<Map<String, BaseValue>, BaseResult>> tryExecuteUnchecked(Map<String, BaseValue> env) {
         return Try.of(() -> executeUnchecked(env));
     }
 
     default Tuple2<Map<String, BaseValue>, BaseResult> execute(Map<? extends String, ? extends BaseValue> env)
-            throws Throwable {
+            throws LineException {
         Objects.requireNonNull(env);
         env.values().forEach(Objects::requireNonNull);
 
